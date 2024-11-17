d7211913b8a1c83d5f89c92620170fc3cc662b86
hjyoonyo <hjyoon2048@gmail.com>
2024-11-18 02:40:36 +0900

    [feat] 책검색 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const userApi = require("./user.api");
 const authApi = require("./auth.api");
 const postApi = require("./post.api");
+const bookApi = require("./book.api");
 
 router.use("/user", userApi);
 router.use("/auth", authApi);
 router.use("/post", postApi);
+router.use("/book", bookApi);
 
 module.exports = router;
