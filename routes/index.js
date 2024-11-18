d8f157e0b686d4fab4d2a88815b1ec0fd9803b89
bjKim2 <rlaxodjs3@naver.com>
2024-11-18 15:54:04 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userApi = require("./user.api");
 const authApi = require("./auth.api");
+const gptApi = require("./gpt.api");
 
 router.use("/user", userApi);
 router.use("/auth", authApi);
+router.use("/gpt", gptApi);
 
 module.exports = router;
