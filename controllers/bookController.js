618066330d5fec03248558b61c92e2aee630ccc5
Eunseo Kim <dmstj9408@gamil.com>
2024-11-18 17:36:37 +0900

    [hotfix]fetch error 수정

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -27,7 +27,7 @@ bookController.searchBooks = async (req, res) => {
             thumbnail: item.thumbnail,
             publisher: item.publisher,
         }));
-        
+
         res.json({ results });
     } catch (error) {
         console.error("Kakao API 요청 중 오류 발생:", error.message);
@@ -39,8 +39,14 @@ bookController.getBestSellers = async (req, res) => {
     const ALADIN_API_KEY = process.env.ALADIN_REST_API_KEY; // Kakao REST API Key 환경 변수에서 가져오기
     const url = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101&ttbkey=${ALADIN_API_KEY}&QueryType=Bestseller&Year=2024&Month=11&Week=1`
     console.log('url',url)
-    const response = await axios.get(url)
-
-    console.log('response',response.data);
+        try {
+        const response = await axios.get(url);
+        // console.log('response', response.data);
+        // console.log('length', response.data);
+        res.status(200).json({status: 'success', data: response.data})
+    } catch (error) {
+        console.error('Error fetching bestsellers:', error);
+        res.status(400).json({status: 'fail', error: error.message})
+    }
 }
 module.exports = bookController;
