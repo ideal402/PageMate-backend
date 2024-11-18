d974a2498f54534ba5d6bf4476c25ce92504cdf7
hjyoonyo <hjyoon2048@gmail.com>
2024-11-19 01:48:01 +0900

    [add] 댓글 스키마 postId 추가

--- models/Comment.js
+++ models/Comment.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const commentSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    postId: { type: Schema.Types.ObjectId, ref: "Post", required: true },
     commentText: { type: String, required: true },
     commentDate: { type: Date, default: Date.now },
   },
