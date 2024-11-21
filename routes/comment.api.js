8848b79eff69d20a847bef220cb05d32f0afb1a6
hjyoonyo <hjyoon2048@gmail.com>
2024-11-21 19:32:42 +0900

    [fix] 댓글 로그인 유저만 등록, 삭제 가능하게 주석 해제

--- routes/comment.api.js
+++ routes/comment.api.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const commentController = require("../controllers/commentController");
 const authController = require("../controllers/authController");
 
-// router.post("/write", authController.authenticate, commentController.addComment);
-router.post("/write", commentController.addComment);
+router.post("/write", authController.authenticate, commentController.addComment);
 router.get("/:postId", commentController.fetchComments);
-// router.delete("/:commentId", authController.authenticate, commentController.deleteComment);
-router.delete("/:commentId",commentController.deleteComment);
+router.delete("/:commentId", authController.authenticate, commentController.deleteComment);
 
 module.exports = router;
